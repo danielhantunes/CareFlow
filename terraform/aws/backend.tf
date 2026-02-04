cffe286e88a50e50755f4287d900a4acd5d4750a
Daniel Henrique <danielh.engenhariadesistemas@gmail.com>
2026-02-04 19:43:23 -0300

    Read backend settings from Actions variables

--- terraform/aws/backend.tf
+++ terraform/aws/backend.tf
@@ -1,9 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "careflow-terraform-state-767397900909"
     key            = "aws/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-state-locks"
     encrypt        = true
   }
 }
